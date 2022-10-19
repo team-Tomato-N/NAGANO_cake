554d4c3efdec861c2b9f0c2ec64fb405acf0afe5
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 08:33:15 +0000

    [update]shopping_addresses route(index,create,destroy)設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  def index
-  if params[:id].present?
-    @shopping_addresses = Shopping_addresses.all
-    @shopping_addresses = Shopping_addresses.all
-  else
-    @shopping_addresses = Shopping_addresses.all
-    @shopping_address= Shopping_address.new
-  end
-
-  def create
-    shopping_address = Shopping_address.find(params[:shopping_address_id])
-    shopping_address.save
-    redirect_to shopping_addresses_path
-  end
-
-  def destroy
-    Shopping_address.find(params[:id]).destroy
-    redirect_to shopping_addresses_path
-  end
-
-  private
-  def shopping_address_params
-    params.require(:shopping_address).permit(:postal_code, address, name)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
 
-  namespace :public do
-    get 'shopping_addresses', to: 'shopping_addresses#index'
-    post 'shopping_addresses', to: 'shopping_addresses#create'
-    delete 'shopping_addresses/:id', to: 'shopping_addresses#destroy'
-  end
+  #get以下がURLの最後の記述で、to:いかがフォルダ→コントローラ名→アクション名
+  get 'shopping_addresses', to: 'public/shopping_addresses#index'
+  post 'shopping_addresses', to: 'public/shopping_addresses#create'
+  delete 'shopping_addresses/:id', to: 'public/shopping_addresses#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
