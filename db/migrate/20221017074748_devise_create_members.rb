f956ca9a5593bbb196ea4f1df706ea72e0133b2e
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 06:46:25 +0000

    [update]mamber_db修正

--- db/migrate/20221017074748_devise_create_members.rb
+++ db/migrate/20221017074748_devise_create_members.rb
@@ -38,7 +38,7 @@ class DeviseCreateMembers < ActiveRecord::Migration[6.1]
       t.string :postal_code, null: false
       t.string :address, null: false
       t.string :phone_number, null: false
-      t.boolean :is_deleted, null: false
+      t.boolean :is_deleted, null: false, default:false
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Member.create!(
+  last_name:'試作',
+  first_name:'太郎',
+  last_furigana:'シサク',
+  first_furigana:'タロウ',
+  email:'test@webcamp.jp',
+  postal_code:'1111111',
+  address :'テスト県テスト市1-11',
+  password:'11111'
+)
