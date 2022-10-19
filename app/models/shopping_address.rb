cd84f2257c9c515d9d0a1a2760efc721747a4829
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 07:33:19 +0000

    [update]shopping_addresses Model設定

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -1,2 +1,9 @@
 class ShoppingAddress < ApplicationRecord
+
+  belongs_to :member
+
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
