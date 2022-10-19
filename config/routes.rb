9ed5ceaedc2dcf9d2b6ec4c6d45a14dc579a780a
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 11:50:49 +0000

    [update]routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
 
+  scope module: :public do
+  resources :shopping_addresses
+  end
+
   #get以下がURLの最後の記述で、to:いかがフォルダ→コントローラ名→アクション名
-  get 'shopping_addresses', to: 'public/shopping_addresses#index'
-  post 'shopping_addresses', to: 'public/shopping_addresses#create'
-  delete 'shopping_addresses/:id', to: 'public/shopping_addresses#destroy'
-  get 'shopping_addresses/:id/edit', to: 'public/shopping_addresses#edit'
+  #get 'shopping_addresses', to: 'public/shopping_addresses#index'
+  #post 'shopping_addresses', to: 'public/shopping_addresses#create'
+  #delete 'shopping_addresses/:id', to: 'public/shopping_addresses#destroy', as:"shopping_address"
+  #get 'shopping_addresses/:id/edit', to: 'public/shopping_addresses#edit', as:"edit_shopping_address"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
