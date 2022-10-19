d385eb2d34aeca11e55cc2546bbad40f043c7e88
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 07:46:36 +0000

    [update]shopping_addresses route(index,create,destroy)設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'shopping_addresses/index'
-    get 'shopping_addresses/edit'
-  end
   devise_for :admins
   devise_for :members
+
+  namespace :public do
+    get 'shopping_addresses', to: 'shopping_addresses#index'
+    post 'shopping_addresses', to: 'shopping_addresses#create'
+    delete 'shopping_addresses/:id', to: 'shopping_addresses#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
