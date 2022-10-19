e4c24e063578d934a3d45acbfe14afc7ac249325
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 10:31:21 +0000

    [update]shopping_addresses index画面作成

--- app/controllers/public/shopping_addresses_controller.rb
+++ app/controllers/public/shopping_addresses_controller.rb
@@ -1,7 +1,28 @@
 class Public::ShoppingAddressesController < ApplicationController
   def index
+      @shopping_addresses = ShoppingAddress.all
+      @shopping_address = ShoppingAddress.new
+  end
+
+  def create
+    shopping_address = ShoppingAddress.find(params[:shopping_address_id])
+    #shopping_address.member_id = Member.find(2).id
+    shopping_address.save
+    redirect_to shopping_addresses_path
+  end
+
+  def destroy
+    ShoppingAddress.find(params[:id]).destroy
+    redirect_to shopping_addresses_path
   end
 
   def edit
   end
+
+
+  private
+  def shopping_address_params
+    params.require(:shopping_address).permit(:postal_code, :address, :name)
+  end
+
 end
