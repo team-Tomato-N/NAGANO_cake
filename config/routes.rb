e8a4be203ab745cbf5081f1ae4bfee743f02426b
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-19 07:30:54 +0000

    [update]shopping_addresses db作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'shopping_addresses/index'
+    get 'shopping_addresses/edit'
+  end
   devise_for :admins
   devise_for :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
