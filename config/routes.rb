193356bd0ee20a54a380fd4c03a1cea07c92587a
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-10-20 04:46:18 +0000

    [update]ordersコントローラー作成・ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
   devise_for :members
 
   scope module: :public do
   resources :shopping_addresses
+  resources :orders, only: [:new, :create, :show, :index]
+  post "/orders/confirm" => "orders#confirm"
+  get "/orders/complete" => "orders#complete"
   end
 
   #get以下がURLの最後の記述で、to:いかがフォルダ→コントローラ名→アクション名
