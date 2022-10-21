17d88785e42b327f5b69f64a706ffe385f225ff2
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-21 14:05:08 +0000

    [update]admin-devise routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
   devise_for :members,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
