352180123a1a8527f357e91ed75a5901e9276226
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-24 04:37:34 +0000

    [update]members-mypage 一時保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,7 @@ class ApplicationController < ActionController::Base
       end
   end
 
-  def
-  after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(resource_or_scope)
       if resource_or_scope == :member
           root_path
       elsif resource_or_scope == :admin
