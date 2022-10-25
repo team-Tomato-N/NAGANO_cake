990971c6bee27207527cdf7ad93846b12f94c26f
EC2 Default User <ayaka-31014313@outlook.jp>
2022-10-25 10:48:12 +0000

    [update]ジャンル機能実装

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    #@genres = Genre.where(is_active: true)
+    @genres = Genre.all
     @items = Item.all
     @random = Item.order("RANDOM()").limit(4)
   end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
 
-　has_many :items
+  has_many :items
   validates :name, presence: true
 end
