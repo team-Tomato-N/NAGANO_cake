ea0feb0862bf2944cc44577c0e2cd222dd1653c2
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-10-26 11:50:15 +0000

    [update] 商品一覧の新規登録のレイアウト完成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @cart_items = current_member.cart_items
   end
   def show
     @item = Item.find(params[:id])
