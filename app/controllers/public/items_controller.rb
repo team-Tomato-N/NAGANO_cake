8bbc1697865fc9fc9cf1847c5442b86ac7564aa3
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-10-27 08:16:06 +0000

    商品一覧以外のレイアウト完成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,12 +4,14 @@ class Public::ItemsController < ApplicationController
       @genre = Genre.find(params[:genre_id])
       @items = @genre.items
     else
+      @genres = Genre.all
       @items = Item.all
       @cart_items = current_member.cart_items
     end
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
