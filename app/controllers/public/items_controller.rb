65c1f5b307a83606dfcc08de4a009183400d21d4
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-10-27 10:26:38 +0000

    最終確認

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
       @items = @genre.items
     else
-      @genres = Genre.all
       @items = Item.all
-      @cart_items = current_member.cart_items
     end
   end
 
